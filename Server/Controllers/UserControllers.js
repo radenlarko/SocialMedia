98899f96ad97c2b7b79fab024403f5e4bc878f29
radenlarko <radenlarko@gmail.com>
2022-11-13 15:12:29 +0700

    add route update user

--- Server/Controllers/UserControllers.js
+++ Server/Controllers/UserControllers.js
@@ -1,5 +1,7 @@
+import bcrypt from "bcrypt";
 import UserModel from "../Models/userModel.js";
 
+// get a user
 export const getUser = async (req, res) => {
   const { id } = req.params;
 
@@ -12,6 +14,39 @@ export const getUser = async (req, res) => {
     }
 
     return res.status(404).json({ message: "User does not exist!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// update a user
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const {
+    currentUserId,
+    currentUserAdminStatus,
+    password: passBody,
+  } = req.body;
+
+  try {
+    if (id === currentUserId || currentUserAdminStatus) {
+      if (passBody) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(passBody, salt);
+        req.body.password = hashedPass;
+      }
+
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      const { password, ...rest } = user._doc;
+      return res.status(200).json({ success: true, data: rest });
+    }
+
+    return res.status(403).json({
+      success: false,
+      message: "Access denied! you can only update your own profile!",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getUser } from "../Controllers/UserControllers.js";
+import { getUser, updateUser } from "../Controllers/UserControllers.js";
 
 const router = express.Router();
 router.get("/:id", getUser);
+router.put("/:id", updateUser);
 
 export default router;
