55ad98d4fd8b773372f0f66d2eaf0f29acf3257f
radenlarko <radenlarko@gmail.com>
2022-11-13 13:34:09 +0700

    add route login user

--- Server/Controllers/AuthControllers.js
+++ Server/Controllers/AuthControllers.js
@@ -18,8 +18,29 @@ export const registerUser = async (req, res) => {
   try {
     await newUser.save();
     res.status(200).json({ success: true, data: newUser });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-    console.log(err);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Login user
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+
+      const validate = validity
+        ? res.status(200).json(user)
+        : res.status(400).json({ message: "wrong password" });
+      return validate;
+    }
+
+    return res.status(404).json({ message: "User does not exist!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
