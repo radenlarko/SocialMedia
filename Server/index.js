5a53c1a0d6c24bdb8efdd87235bf64402a6a7839
radenlarko <radenlarko@gmail.com>
2022-12-06 15:21:34 +0700

    Add server route and model Chat

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,7 @@ import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
 import PostRoute from "./Routes/PostRoute.js";
 import UploadRoute from "./Routes/UploadRoute.js";
+import ChatRoute from "./Routes/ChatRoute.js";
 
 dotenv.config({ path: ".env.local" }); // change to dotenv.config() for production and setup .env file
 
@@ -40,3 +41,4 @@ app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
 app.use("/post", PostRoute);
 app.use("/upload", UploadRoute);
+app.use("/chat", ChatRoute);
